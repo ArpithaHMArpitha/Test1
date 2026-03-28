dbb7e5ced7a529b19594c79a59ee4d77d8fbdd46
Arpitha <arpithasujatha@gmail.com>
2026-03-28 11:09:35 +0530

    added a.java in master

---
+++ a.java
@@ -0,0 +1,20 @@
+package com;
+
+class MethodExample1 {
+	//1. Method without argument, without return statement
+	
+	public void test() {
+		System.out.println("learning methods");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Start");
+		System.out.println("---------");
+		MethodExample1 me1 = new MethodExample1();
+		me1.test();
+		System.out.println("---------");
+		System.out.println("End");
+		
+	}
+
+}
